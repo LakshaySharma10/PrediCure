0c2621620579781e15a8ecc0f78997015937ac03
Kunal Bansal <158471656+KunalBA@users.noreply.github.com>
2024-03-15 19:58:53 +0530

    Add files via upload

---
+++ website/style.js
@@ -0,0 +1,35 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+document.addEventListener("keydown", function(event) {
+  if (event.key === "ArrowRight") {
+    plusSlides(1); // Call plusSlides function with 1 to move to the next slide
+  } else if (event.key === "ArrowLeft") {
+    plusSlides(-1); // Call plusSlides function with -1 to move to the previous slide
+  }
+});
